5f5554b6c33e30bee13952e61afe92130356d918
Kaushik Kishore <Kaushik.Kishore@wdc.com>
2019-09-23 16:16:42 +0530

    Angular Udemy Tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { WarningAlert } from "./warning-alert/waring-alert.component";
+import { AlertSuccessComponent } from './alert-success/alert-success.component';
+import { AlertDangerComponent } from './alert-danger/alert-danger.component';
+import { AlertInfoComponent } from './alert-info/alert-info.component';
+import { AlertsComponent } from './alerts/alerts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, WarningAlert, AlertSuccessComponent, AlertDangerComponent, AlertInfoComponent, AlertsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/warning-alert/waring-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-warning-alert",
+  templateUrl: "./warning-alert.component.html",
+  styles: [
+    `
+      .large {
+        font-size: 20px;
+      }
+    `
+  ]
+})
+export class WarningAlert {}
